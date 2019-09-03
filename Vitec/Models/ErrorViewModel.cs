0c2e323a93e908348242bdadd3cc9d52e2c658a9
Zxited <mmgamesfull@gmail.com>
2019-09-03 10:45:09 +0200

    Setup af projekt

---
+++ Vitec/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vitec.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
