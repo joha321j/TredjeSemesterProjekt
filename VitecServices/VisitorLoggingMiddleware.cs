80ad57ce0f10e6ff644b12d76152d1d2bec8119c
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2019-12-06 09:14:08 +0100

    Create VisitorLoggingMiddleware.cs

---
+++ VitecServices/VisitorLoggingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace VitecServices
+{
+    public class VisitorLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public VisitorLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
